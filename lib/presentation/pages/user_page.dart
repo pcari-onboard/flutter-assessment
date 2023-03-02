caa14f0167a56a2b590de7159fbd01bd8e832aeb
Ahmad-Elraqab <65804256+Ahmad-Elraqab@users.noreply.github.com>
2023-03-02 14:53:46 +0800

    clean architecture setup

---
+++ lib/presentation/pages/user_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactsPage extends StatefulWidget {
+  @override
+  createState() => _ContactsPage();
+}
+
+class _ContactsPage extends State<ContactsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
